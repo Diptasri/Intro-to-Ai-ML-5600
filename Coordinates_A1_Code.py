ee0608f65cbde9e7beeb5bdb78e8e30655e2c763
Diptasri <89195697+Diptasri@users.noreply.github.com>
2021-08-24 19:06:42 +0530

    Add files via upload

---
+++ Coordinates_A1_Code.py
@@ -0,0 +1,20 @@
+
+# inputs
+a = float (input ("Enter a value: "))
+b = float (input ("Enter b value: "))
+
+# Internal division section formula
+Px = ((a * a) + (b * b)) / (a + b)
+Py = ((a * a) + (2 * a * b) - (b * b)) / (a + b)
+
+# External division section formula
+Qx = ((a * a) - (2 * a * b) - (b * b)) / (a - b)
+Qy = ((a * a) + (b * b)) / (a - b)
+
+# Final coordinates
+internal_coordinates = (Px, Py)
+external_coordinates = (Qx, Qy)
+
+# Printing outputs
+print (internal_coordinates)
+print (external_coordinates)
